e4a89619057d9bbadef6ea6950c8fe1705e94b9b
Krajcik Marina <120725089+Kekso7@users.noreply.github.com>
2025-11-26 09:00:31 +0100

    Add files via upload

---
+++ JS11/function.js
@@ -0,0 +1,54 @@
+
+
+function sayHello(){
+    console.log("Hello!");
+}
+
+sayHello();
+sayHello();
+
+function online(){
+    console.log("Sajt je online!");
+}
+online();
+
+
+function isAdult(age){
+    console.log(age >= 18 ? "Adult" : "Not adult");
+}
+
+let toma = 55;
+
+isAdult(15);
+isAdult(18);
+isAdult(toma);
+
+function writeFullName(firstName, lastName){
+    console.log(firstName+" "+lastName);
+}
+writeFullName("Marina", "Krajcik");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
